6b34fd1077b9efedcb89c47ff23d4fc41707ce95
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-09 13:51:22 -0300

    styled-components

---
+++ styled-components/src/App.tsx
@@ -0,0 +1,17 @@
+import { Title, TitleSmall } from './styles'
+
+function App() {
+  return (
+    <div>
+      <Title fontSize={30}>
+        Hello, World!
+        <br />
+        <span>Text Menor</span>
+      </Title>
+
+      <TitleSmall>Menor</TitleSmall>
+    </div>
+  )
+}
+
+export default App
