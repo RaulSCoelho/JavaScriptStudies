867531dce20dc2341ba1e6a1c73982d26cf118ba
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-11 04:52:38 -0300

    fixed

--- themeswitcher/src/styles/global.ts
+++ themeswitcher/src/styles/global.ts
@@ -8,9 +8,9 @@ export default createGlobalStyle`
   }
 
   body{
-    background: ${(props) => props.theme.colors.background};
+    background: ${props => props.theme.colors.background};
     font-size: 14px;
-    color: ${(props) => props.theme.colors.text};
+    color: ${props => props.theme.colors.text};
     font-family: sans-serif;
   }
 `
