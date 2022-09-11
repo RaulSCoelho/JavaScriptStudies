874b6c0fddbf5b004efbba96871afa5c1978f4a7
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-11 20:37:03 -0300

    serializers

--- Djando-RestAPI/backend/api/views.py
+++ Djando-RestAPI/backend/api/views.py
@@ -3,11 +3,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
-  model_data = Product.objects.all().order_by("?").first()
+  instance = Product.objects.all().order_by("?").first()
   data = {}
-  if model_data:
-    data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+  if instance:
+    # data = model_to_dict(instance, fields=['id', 'title', 'price'])
+    data = ProductSerializer(instance).data
   return Response(data)
