f941c9924c57895a5864d0b0406b647b220eb03e
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-11 19:49:42 -0300

    echo data

--- Djando-RestAPI/py_client/basic.py
+++ Djando-RestAPI/py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello World"}) # HTTP REQUEST
+get_response = requests.get(endpoint, params={"product_id": 123}) # HTTP REQUEST
 print(get_response.text) # print raw text response
 
 print(get_response.status_code) # print status code
