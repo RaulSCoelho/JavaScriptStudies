d85f749a1314337a6c246626fda882d1bc848dff
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-11 18:57:58 -0300

    initialize

--- Djando-RestAPI/py_client/basic.py
+++ Djando-RestAPI/py_client/basic.py
@@ -1,3 +1,10 @@
-import requests;
+import requests
 
-endpoint = ''
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint, json={"query": "Hello World"}) # HTTP REQUEST
+print(get_response.text) # print raw text response
+
+print(get_response.json()) # print json response
+
+print(get_response.status_code) # print status code
