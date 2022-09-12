bc8fb509a29dd66c098f7adb9c7f51421cf97bf3
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-11 22:57:30 -0300

    CRUD

--- Djando-RestAPI/backend/products/urls.py
+++ Djando-RestAPI/backend/products/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.ProductListCreateAPIView.as_view()),
-  path('<int:pk>/', views.ProductDetailAPIView.as_view())
+  path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+  path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+  path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view())
 ]
--- Djando-RestAPI/backend/products/views.py
+++ Djando-RestAPI/backend/products/views.py
@@ -26,6 +26,25 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
   # lookup_field = 'pk' ??
 
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = 'pk'
+
+  def perform_update(self, serializer):
+    instance = serializer.save()
+    if not instance.content:
+      instance.content = instance.title
+
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = 'pk'
+
+  def perform_destroy(self, instance):
+    super().perform_destroy(instance)
+
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
 
