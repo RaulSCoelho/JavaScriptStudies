dea6ac53b630bda4d6614e0a3b57817d4d7fd356
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-15 20:10:14 -0300

    tokens

--- Djando-RestAPI/backend/api/urls.py
+++ Djando-RestAPI/backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('', views.api_home), # localhost:8000/api/
-    path('products/', include('products.urls')),
+  path('auth/', obtain_auth_token),
+  path('', views.api_home), # localhost:8000/api/
+  path('products/', include('products.urls')),
 ]
--- Djando-RestAPI/backend/products/views.py
+++ Djando-RestAPI/backend/products/views.py
@@ -1,7 +1,9 @@
-from rest_framework import authentication, generics, mixins, permissions
+from rest_framework import authentication ,generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+
+from api.authentication import TokenAuthentication
 
 from .models import Product
 from .permissions import IsStaffEditorPermission
@@ -11,7 +13,7 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
-  authentication_classes = [authentication.SessionAuthentication]
+  authentication_classes = [authentication.SessionAuthentication, TokenAuthentication]
   permission_classes = [permissions.IsAdminUser ,IsStaffEditorPermission]
 
   def perform_create(self, serializer):
