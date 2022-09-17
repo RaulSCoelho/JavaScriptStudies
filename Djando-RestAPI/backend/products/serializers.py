ba4646d12f653d8dcada8463f579412943a57c68
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-17 12:00:43 -0300

    urls

--- Djando-RestAPI/backend/products/serializers.py
+++ Djando-RestAPI/backend/products/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
+
+def get_url(self, obj, url_name):
+  request = self.context.get('request')
+  if request is None:
+    return None
+
+  return reverse(url_name, kwargs={'pk': obj.pk}, request=request)
 
 class ProductSerializer(serializers.ModelSerializer):
   my_discount = serializers.SerializerMethodField(read_only=True)
+  detail_url = serializers.HyperlinkedIdentityField(view_name='product-detail')
+  edit_url = serializers.SerializerMethodField(read_only=True)
   class Meta:
     model = Product
     fields = [
+      'detail_url',
+      'edit_url',
       'id',
       'title',
       'content',
@@ -14,6 +25,9 @@ class ProductSerializer(serializers.ModelSerializer):
       'sale_price',
       'my_discount'
     ]
+
+  def get_edit_url(self,obj):
+    return get_url(self, obj, 'product-update')
 
   def get_my_discount(self, obj):
     if not hasattr(obj, 'id'):
--- Djando-RestAPI/backend/products/urls.py
+++ Djando-RestAPI/backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.ProductListCreateAPIView.as_view()),
-  path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-  path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
-  path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view())
+  path('', views.ProductListCreateAPIView.as_view(), name='product-list'),
+  path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+  path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-update'),
+  path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view(), name='product-delete')
 ]
