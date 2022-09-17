d9569721f876a49b49fb5c264042a0ec31641ede
RaulSCoelho <raul.s.coelho@gmail.com>
2022-09-17 16:01:41 -0300

    request user data

--- Djando-RestAPI/backend/products/serializers.py
+++ Djando-RestAPI/backend/products/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
+
+from .validators import validate_title_no_hello, unique_product_title
 from .models import Product
 
 def get_url(self, obj, url_name):
@@ -13,30 +15,40 @@ class ProductSerializer(serializers.ModelSerializer):
   my_discount = serializers.SerializerMethodField(read_only=True)
   detail_url = serializers.HyperlinkedIdentityField(view_name='product-detail')
   edit_url = serializers.SerializerMethodField(read_only=True)
-  email = serializers.EmailField(write_only=True)
+  title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+  # name = serializers.CharField(source='title', read_only=True)
   class Meta:
     model = Product
     fields = [
+      # 'user',
       'detail_url',
       'edit_url',
-      'email',
       'id',
       'title',
+      # 'name',
       'content',
       'price',
       'sale_price',
       'my_discount'
     ]
 
-  def create(self, validated_data):
-    # email = validated_data.pop('email')
-    obj = super().create(validated_data)
-    # print(email, obj)
-    return obj
+  # def validate_title(self, value):
+  #   request = self.context.get('request')
+  #   user = request.user
+  #   qs = Product.objects.filter(user=user, title__iexact=value)
+  #   if qs.exists():
+  #     raise serializers.ValidationError(f"{value} is already a product name.")
+  #   return value
 
-  def update(self, instance, validated_data):
-    email = validated_data.pop('email')
-    return super().update(instance, validated_data)
+  # def create(self, validated_data):
+  #   # email = validated_data.pop('email')
+  #   obj = super().create(validated_data)
+  #   # print(email, obj)
+  #   return obj
+
+  # def update(self, instance, validated_data):
+  #   email = validated_data.pop('email')
+  #   return super().update(instance, validated_data)
 
   def get_edit_url(self,obj):
     return get_url(self, obj, 'product-update')
--- Djando-RestAPI/backend/products/views.py
+++ Djando-RestAPI/backend/products/views.py
@@ -4,48 +4,74 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Product
-from api.mixins import StaffEditorPermissionMixin
+from api.mixins import StaffEditorPermissionMixin, UserQuerySetMixin
 from .serializers import ProductSerializer
 
 # region FIRST WAY YOU CAN BUILD YOUR VIEW
-class ProductListCreateAPIView(StaffEditorPermissionMixin, generics.ListCreateAPIView):
+
+# region CREATE OR LIST PRODUCTS
+class ProductListCreateAPIView(
+  UserQuerySetMixin,
+  StaffEditorPermissionMixin,
+  generics.ListCreateAPIView):
+
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
   def perform_create(self, serializer):
     # serializer.save(user=self.request.user)
-    email = serializer.validated_data.pop('email')
-    print(email)
     title = serializer.validated_data.get('title')
     content = serializer.validated_data.get('content') or None
     if content is None:
       content = title
-    serializer.save(content=content)
+    serializer.save(user=self.request.user, content=content)
 
+  # def get_queryset(self, *args, **kwargs):
+  #   qs = super().get_queryset(*args, **kwargs)
+  #   request = self.request
+  #   return qs.filter(user=request.user)
+# endregion
 
-class ProductDetailAPIView(StaffEditorPermissionMixin, generics.RetrieveAPIView):
+# region PRINT THE PRODUCT DETAILS
+class ProductDetailAPIView(
+  UserQuerySetMixin,
+  StaffEditorPermissionMixin,
+  generics.RetrieveAPIView):
+
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+# endregion
 
+# region UPDATE A PRODUCT
+class ProductUpdateAPIView(
+  UserQuerySetMixin,
+  StaffEditorPermissionMixin,
+  generics.UpdateAPIView):
 
-class ProductUpdateAPIView(StaffEditorPermissionMixin, generics.UpdateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
-  lookup_field = 'pk'
+  lookup_field = 'pk' # default
 
   def perform_update(self, serializer):
     instance = serializer.save()
     if not instance.content:
       instance.content = instance.title
+# endregion
 
+# region DELETE A PRODUCT
+class ProductDeleteAPIView(
+  UserQuerySetMixin,
+  StaffEditorPermissionMixin,
+  generics.DestroyAPIView):
 
-class ProductDeleteAPIView(StaffEditorPermissionMixin, generics.DestroyAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
-  lookup_field = 'pk'
+  lookup_field = 'pk' # default
 
   def perform_destroy(self, instance):
     super().perform_destroy(instance)
+# endregion
+
 # endregion
 
 # region SECOND WAY YOU CAN BUILD YOUR VIEW
