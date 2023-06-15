e8e7377df1f0896534045aa91eea7897e2df3800
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-06-15 12:45:32 -0300

    sentry

---
+++ firebase/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f943e82fa66144e5859333cd06e827f7@o4505364205469696.ingest.sentry.io/4505364205600768",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
