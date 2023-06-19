468e2137445b31b42a5610719c8cecd421d621f4
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-06-19 16:51:35 -0300

    loading

--- firebase/src/components/Sentry/RaiseErrors.tsx
+++ firebase/src/components/Sentry/RaiseErrors.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "../Buttons";
+import Loading from "@/app/loading";
 
 export function RaiseErrors() {
+  const [isLoading, setIsLoading] = useState(false);
+
   function handleFrontEndError() {
     throw new Error("Front-end error raised from button click");
   }
 
   async function handleBackEndError() {
+    setIsLoading(true);
     await fetch("api/sentry");
+    setIsLoading(false);
   }
 
   return (
     <div>
+      {isLoading && <Loading />}
       <h2 className="mb-4 text-2xl font-bold">Sentry</h2>
       <div className="flex flex-col gap-4 sm:flex-row">
         <Button onClick={handleFrontEndError}>Front-End Error</Button>
